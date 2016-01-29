326e14c64a108a206892f0998a52f714bc2ce045
Mohamed Saleem <mohsal.saleem@gmail.com>
2016-01-30 02:09:34 +0530

    Code clean up

--- hack01.rb
+++ hack01.rb
@@ -1,57 +1,84 @@
-
 def input
 	user_input = gets.chomp
 	user_input
 end
 
 def is_input_valid?(input_string)
-	if input_string =~ /\d/ or input_string =~ /\s/
+
+	special = "?<>',?[]}{=-)(*&^%$#`~{}."
+	regex = /[#{special.gsub(/./){|char| "\\#{char}"}}]/
+
+	#Check for presence of numbers or whitespaces or specialcharacters 
+	if input_string =~ /\d/ or input_string =~ /\s/ or input_string =~ regex 
 		false
+
+	#Check for length constraint 
 	elsif input_string.length < 2 or input_string.length > 7
 		false
 	else
-		true
+		#otherwise 
+		true 
 	end
 end
 
-puts "Enter a string no less than 2 letters and no more than 7 letters.."
+def sort_string(string)
+	string.chars.sort { |a, b| a.casecmp(b) }.join
+end
+
+#Function to contruct the words list into hash with words as keys with alphabetically sorted words as values
+def construct_words_hash
+	hash = {}
+		File.open('wordsEn.txt','r').each_line do |line|
+			word = line.split(/\t/)
+			sorted_word = sort_string(word[0].chomp)
+			hash[word[0].chomp] = sorted_word
+		end
+	return hash
+end
+
+def compute(string)
+	sorted_string = sort_string(string)
+	words_hash = construct_words_hash
+	sorted_hash = words_hash.sort.to_h
+	output_array = []
+	if sorted_string.include?('a')
+		output_array.push('a')
+	end
+	output_array.push(sorted_hash.select{|k,v| v == sorted_string }.map{|i| i[0] })
+	temp = sorted_string.split('')
+	sorted_string.split('').each_with_index do |i,index|
+		s = ""
+		#Go through the hash to find those meaningful words
+		for j in 0..string.length-1
+			if j != index
+				s += sorted_string[j]
+				found_words = sorted_hash.select{|k,v| v == s }.map{|i| i[0] }# Will return an array with found words
+				output_array.push(found_words)
+			end
+		end
+	end
+	return output_array
+end
+
+if File.file?('wordsEn.txt') and File.readable?('wordsEn.txt')
+	puts "Legal words file found.."
+else
+	puts "Legal words file not found. Please place it in the same folder as this program and name it to wordsEn.txt . Exiting.."
+	exit
+end
+#User enters the word
+puts "Enter a string with length no less than 2 letters and no more than 7 letters.."
 user_input = input
+
+#Check for validity of the word
 while !is_input_valid?(user_input)
 	puts "Not a valid input. Enter again"
 	user_input = input
 end
-sorted_user_input = user_input.chars.sort { |a, b| a.casecmp(b) } .join
 
-words_hash = {}
+puts "You have entered: "+user_input
 
-File.open('wordsEn.txt','r').each_line do |line|
-	word = line.split(/\t/)
-	sorted_word = word[0].chomp.chars.sort { |a, b| a.casecmp(b) } .join
-	words_hash[word[0].chomp] = sorted_word
-end
-
-sorted_hash = words_hash.sort.to_h
-
-output_array = []
-
-puts "Output"
-
-if sorted_user_input.include?('a')
-	output_array.push('a')
-end
-
-output_array.push(sorted_hash.select{|k,v| v == sorted_user_input }.map{|i| i[0] })
-
-temp = sorted_user_input.split('')
-
-sorted_user_input.split('').each_with_index do |i,index|
-	s = ""
-	for j in 0..user_input.length-1
-		if j != index
-			s += sorted_user_input[j]
-		end
-	end
-	output_array.push(sorted_hash.select{|k,v| v == s }.map{|i| i[0] })
-end
-
-puts output_array.flatten.sort
+#Compute the output
+output = compute(user_input)
+puts output.flatten.sort.uniq # Make the recieved array into a single array(flatten),
+							  #  sort it, remove dupicates and show the output
